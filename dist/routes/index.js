e8407ade1809405ad80575c297e1c947b7d68378
odutradev <joao.vitornl@gmail.com>
2026-02-23 16:51:00 -0300

    chore: Removing unnecessary files

--- dist/routes/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.apiRouter = void 0;
-const express_1 = require("express");
-const health_routes_1 = require("./health.routes");
-exports.apiRouter = (0, express_1.Router)();
-exports.apiRouter.use('/health', health_routes_1.healthRouter);
--- dist/services/health.service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getSystemHealth = void 0;
-const getSystemHealth = () => ({
-    status: 'healthy',
-    timestamp: new Date().toISOString(),
-    uptime: process.uptime()
-});
-exports.getSystemHealth = getSystemHealth;
