7fa9b87d1011a6b8fc0b48e629978f1ac2d8b852
odutradev <joao.vitornl@gmail.com>
2026-02-24 11:47:23 -0300

    feat: Creating router controller

---
+++ src/routes/index.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+
+import controlAccess from "@middlewares/controlAccess";
+// import kvRouter from "./resources/kv.router";
+
+const router = Router();
+
+router.get("/ping", (req, res) => {
+    res.sendStatus(200);
+});
+
+router.get("/validate/control-access", controlAccess, (req, res) => {
+    res.send(`Authorization as "${res.locals.projectID}"`).status(200);
+});
+
+
+// router.use("/kv", [controlAccess],  kvRouter);
+
+export default router;
